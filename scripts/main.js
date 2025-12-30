2bce2c9e6f734779cd947256e0ff690c2cae8bea
FakhridhoGunawan73 <fakhridhogunawan01@gmail.com>
2025-12-30 13:17:30 +0700

    feat: update logic and UI for clicker and number guessing games

--- scripts/main.js
+++ scripts/main.js
@@ -7,9 +7,6 @@ function handleMenuToggleClick() {
     mobileMenu.classList.toggle('hidden');
     iconOpen.classList.toggle('hidden');
     iconClose.classList.toggle('hidden');
-    console.log('Menu Mobile Hidden Status:', mobileMenu.classList.contains('hidden'));
-    console.log('Ikon Garis Hidden Status:', iconOpen.classList.contains('hidden'));
-    console.log('Ikon X Hidden Status:', iconClose.classList.contains('hidden'));
 }
 
 menuToggle.addEventListener('click', handleMenuToggleClick);
